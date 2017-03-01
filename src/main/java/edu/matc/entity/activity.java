f213f11fcf116a91f8e4b931db6aa090ca6d1f08
ocollins <ocollins@madisoncollege.edu>
2017-02-28 21:42:28 -0600

    adding Maven project

---
+++ src/main/java/edu/matc/entity/activity.java
@@ -0,0 +1,9 @@
+package edu.matc.entity;
+
+/**
+ * Created by student on 2/28/17.
+ *
+ * @author O Collins
+ */
+public class activity {
+}
---
+++ src/main/java/edu/matc/persistence/ActivityDao.java
@@ -0,0 +1,9 @@
+package edu.matc.persistence;
+
+/**
+ * Created by student on 2/28/17.
+ *
+ * @author O Collins
+ */
+public class ActivityDao {
+}
